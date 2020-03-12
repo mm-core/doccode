06e948122a63bfaba7c714bb13720bd9a7ad3991
taoqf <tao_qf@163.com>
2020-03-12 15:49:25 +0800

    1.0

--- readme.md
+++ readme.md
@@ -1,28 +1,99 @@
-# Base
+# 编码服务
 
-## Projects
+## Description
 
-- amd
-	- on
-	- global
-	- web
-- cjs
-	- config
-	- invoke
-	- server
+生成唯一全局编码。
 
-## Branches
+编码配置均需要有前缀，同时前缀也作为编码的名称，即数据库中`id`字段值。该服务免配置，但也可以在数据库中对配置表进行修改.
 
-- amd
-	- tpl
-		- tpl-web
-		- tpl-wxapp
-		- tpl-mobile
-	- atom-web
-	- atom-wxapp
-	- widgets
-- cjs
-	- atom-nodejs
-	- atom-mobile
-	- widgets-mobile
-	- widgets-wxapp
+## 注意
+
+服务不可部署多个实例
+
+另外，由于该服务生成编号时为了保证生成的编号唯一，需要排队处理大量的并发请求，所以在使用中，请一定避免多次调用。
+
+## 部署
+
+1. 生成配置表，根据实际部署情况修改
+1. 安装`yarn add @mmstudio/doccode`
+1. 启动`./node_moduels/.bin/mm-doccode`
+
+## Useage
+
+```sh
+curl http://127.0.0.1:8890 -d 'name=foo&num=1'
+# ["foo000001"]
+curl http://127.0.0.1:8890 -d 'name=foo&num=3'
+# ["bar000001","bar000002","bar000003"]
+curl http://127.0.0.1:8890 --header "Content-Type:application/json" -d '{"name":"foo","num:1}'
+# ["foo000002"]
+curl http://127.0.0.1:8890 -d 'name=20200204&num=10&len=3'
+# ["20200204001","20200204002","20200204003","20200204004","20200204005","20200204006","20200204007","20200204008","20200204009","20200204010"]
+curl http://127.0.0.1:8890 -d 'name=20200204&num=10&len=2'
+# ["2020020401","2020020402","2020020403","2020020404","2020020405","2020020406","2020020407","2020020408","2020020409","2020020410"]
+```
+
+经以上调用之后，数据库表如下:
+
+id | len | no
+---|---|---
+foo | 6 | 2
+bar | 6 | 5
+20200204 | 3 | 10
+20200204 | 2 | 10
+
+## Configuration
+
+### mm.json
+
+以下，`sys`为固定配置名
+
+```json
+{
+	"port": 8890,
+	"dbs": {
+		"sys": {
+			"type": "postgres",
+			"source": "postgres://mmstudio:Mmstudio123@127.0.0.1:5432/mmstudio"
+		}
+	}
+}
+```
+
+### log4js.json
+
+```json
+{
+	"appenders": {
+		"out": {
+			"type": "stdout"
+		},
+		"console": {
+			"type": "console"
+		},
+		"dateFile": {
+			"type": "dateFile",
+			"filename": "./logs/mm.log",
+			"maxLogSize": 10240000,
+			"backups": 30
+		}
+	},
+	"categories": {
+		"default": {
+			"appenders": [
+				"console",
+				"dateFile"
+			],
+			"level": "debug"
+		}
+	}
+}
+```
+
+## Table
+
+字段名称|类型|说明
+---|---|---
+id|text|关键字，如果配置表有该项，则使用该项配置，如没有，则自动创建，也作为前缀
+len|smallint|编码长度，仅包含数字长度，不包含前缀长度，默认为6位
+no|bigint|当前编号，自动创建时默认起始编号为1
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,157 @@
-export default function fun() {
+#!/usr/bin/env node
+import config from '@mmstudio/config';
+import an14 from '@mmstudio/an000014';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { configure, getLogger } from 'log4js';
+
+interface IDoccode {
+	id: string;
+	len: number;
+	no: bigint;	// 字段为 bigint
 }
+
+const logger = getLogger();
+let lock = false;
+const db = 'sys';
+
+async function query(name: string, num: number, len: number) {
+	const [[code]] = await an14<IDoccode>(db, [`select id, len, no from doccode where id='${name}' and len=${len}`, []]);
+	if (code) {
+		const no = code.no + BigInt(num);
+		await an14(db, [`update doccode set no=${no} where id='${name}' and len=${len}`, []]);
+		return prefix(name, no, code.len, num);
+	}
+	await an14(db, [`insert into doccode (id, len, no) values('${name}', ${len}, ${num})`, []]);
+	return prefix(name, BigInt(num), len, num);
+}
+
+function prefix(pre: string, stop: bigint, len: number, num: number) {
+	return Array<number>(num).fill(0).map((_v, i) => {
+		const n = stop - BigInt(i);
+		return pre + (Array<string>(len).join('0') + n.toString()).slice(-len);
+	}).reverse();
+}
+
+async function get_next_no(name: string, num: number, len: number) {
+	if (lock) {
+		return new Promise<string[]>((resolve) => {
+			setImmediate(() => {
+				resolve(get_next_no(name, num, len));
+			});
+		});
+	}
+	lock = true;
+	logger.debug('locked', name);
+	try {
+		return await query(name, num, len);
+	} finally {
+		lock = false;
+		logger.debug('unlocked', name);
+	}
+}
+
+async function init_db() {
+	const sql = `CREATE TABLE IF NOT EXISTS doccode
+(
+	id text NOT NULL,
+	len integer,
+	no bigint,
+	CONSTRAINT pk_doccode PRIMARY KEY (id, len)
+)
+WITH (oids = false);
+
+COMMENT ON TABLE doccode IS '编码表';
+COMMENT ON COLUMN doccode.id IS '名称';
+COMMENT ON COLUMN doccode.len IS '编码数字位长度';
+COMMENT ON COLUMN doccode.no IS '编号';`;
+	await an14(db, [sql, []]);
+}
+
+function init_http() {
+	const app = express();
+	// parse application/x-www-form-urlencoded
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}));
+	// parse application/json
+	app.use(bodyParser.json({
+		type: '*/json'
+	}));
+	// parse an HTML body into a string
+	app.use(bodyParser.text({
+		type: 'text/html'
+	}));
+
+	// if (config.acao) {
+	// 	app.use((req, res, next) => {
+	// 		const acao = config.acao === '*' ? req.headers.origin as string : config.acao;
+	// 		res.header('Access-Control-Allow-Origin', acao);
+	// 		res.header('Access-Control-Allow-Headers', 'content-type, x-requested-with');
+	// 		// res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+	// 		res.header('Access-Control-Allow-Methods', 'POST,GET');
+	// 		res.header('Access-Control-Allow-Credentials', 'true');
+	// 		if (config.acma) {
+	// 			res.header('Access-Control-Max-Age', config.acma.toString());	// optons请求有效时间
+	// 		}
+	// 		// res.setHeader('P3P', 'CP=CAO PSA OUR');
+	// 		// res.setHeader('P3P', 'CP=CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA div COM NAV OTC NOI DSP COR');
+	// 		return next();
+	// 	});
+	// }
+
+	const server = app.post('/', async (req, res) => {
+		const headers = req.headers;
+		const body = req.body as { name: string; num: string; len: string };
+		const req_query = req.query as { name: string; num: string; len: string };
+		const name = body.name || req_query.name || req.params.name;
+		const len = body.len || req_query.len || req.params.len;
+		const num = body.num || req_query.num || req.params.num;
+		const tm = new Date();
+		const dbgmsg = `name=${name},num:${num},headers=${JSON.stringify(headers)},body=${JSON.stringify(body)}`;
+		logger.info(`Message incomming:${dbgmsg}`);
+		try {
+			if (!name) {
+				throw new Error('name required');
+			}
+			if (!num) {
+				throw new Error('num required');
+			}
+			const no = await get_next_no(name, num ? parseInt(num, 10) : 1, len ? parseInt(len, 10) : 6);
+			logger.info(`Result:${no} for ${name}`);
+			res.json(no);
+		} catch (err) {
+			logger.trace(err);
+			const e_msg = (err as Error).message;
+			logger.error(`Service Error:${e_msg}`);
+			res.status(500).json({
+				detail: (err as Error).stack,
+				msg: e_msg
+			});
+		} finally {
+			const cost = new Date().getTime() - tm.getTime();
+			if (cost > 500) {
+				logger.error(`Message outgoing:${dbgmsg},Costing ${cost}ms`);
+			} else if (cost > 200) {
+				logger.warn(`Message outgoing:${dbgmsg},Costing ${cost}ms`);
+			} else {
+				logger.info(`Message outgoing:${dbgmsg},Costing ${cost}ms`);
+			}
+		}
+	});
+	server.listen(config.port);
+}
+
+async function main() {
+	process.on('SIGINT', () => {
+		process.exit(0);
+	});
+
+	configure('./log4js.json');
+	logger.warn('Starting doccode service...........^v^');
+	await init_db();
+	init_http();
+	logger.warn(`doccode service is started at port ${config.port}...........^v^`);
+}
+
+main();
